3bb05efee4b624a9cd6ef6d2dda6ca4ba659ee61
axel-ac <abdullahceylan.axel@gmail.com>
2022-06-29 19:32:59 +0300

    Changed

--- app.js
+++ app.js
@@ -7,3 +7,53 @@ let secondNumber = document.getElementById("secondNumber")
 let life = 0
 let chance = 5
 info.innerHTML = `number of attempt: ${life} <br><br> You only have ${chance} chance
+function guessNumber() {
+    let userInput = document.getElementById("input")
+    chance--
+    life++
+    if (chance >= 0) {
+        if (chance == 0) {
+            updateText("You have Lost")
+            info.innerHTML = `number of attempt: ${life} <br><br> You dont have any chances`
+        }
+        else if (userInput.value > 100 || userInput.value < 0 || userInput.value == "") {
+            inputData.innerHTML = `Please enter a number that is lower 100 higher 0`
+            inputData.style.display = "block"
+            
+            chance++
+            life--
+        }
+        else if (number > userInput.value) {
+            inputData.innerHTML = userInput.value
+            userInput.value = ""
+            inputData.style.display = "none"
+            firstNumber.innerHTML = inputData.innerHTML
+            info.innerHTML = `number of attempt: ${life} <br><br> You have only  ${chance} chance`
+        } else if (number < userInput.value) {
+            inputData.innerHTML = userInput.value
+            userInput.value = ""
+            inputData.style.display = "none"
+            secondNumber.innerHTML = inputData.innerHTML
+            info.innerHTML = `number of attempt: ${life} <br><br> You have only ${chance} chance`
+        } else if (number == userInput.value) {
+            updateText("Congratulations, You are the Best");
+            return
+        }
+    }
+}
+function updateText(text) {
+    let delay = 10;
+    let h1 = document.querySelector(".header");
+    h1.innerHTML = text
+        .split("")
+        .map(letter => {
+            console.log(letter);
+            return `<span>` + letter + `</span>`;
+        })
+        .join("");
+    Array.from(h1.children).forEach((span, index) => {
+        setTimeout(() => {
+            span.classList.add("wavy");
+        }, index * 50 + delay);
+    });
+}
